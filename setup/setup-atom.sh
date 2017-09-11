cae9cc0cf800b82c276776d2550b9e4e7e99b68b
Shiv Roychowdhury <sr856@cornell.edu>
2017-09-11 13:53:44 -0400

    Fix Atom setup to include a pause when adding the repository source

--- setup/setup-atom.sh
+++ setup/setup-atom.sh
@@ -1,5 +1,7 @@
 # Install Atom
-add-apt-repository -y ppa:webupd8team/atom
+until add-apt-repository ppa:webupd8team/atom
+do sleep 3
+done
 apt-get -y update
 apt-get install -y atom
 apt-get install -y libxss1
