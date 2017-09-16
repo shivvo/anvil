45d423d04c21f185726927a4d718175de5d2cbb2
Shiv Roychowdhury <sr856@cornell.edu>
2017-09-16 02:22:49 -0400

    Acquire folder permissions for node

--- setup/setup-default.sh
+++ setup/setup-default.sh
@@ -1,4 +1,4 @@
 # Update and upgrade
-apt-get -y update
+apt-get -y --fix-missing update
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 apt-get install -y build-essential git xauth gnome-terminal
--- setup/setup-node-unpriv.sh
+++ setup/setup-node-unpriv.sh
@@ -1,2 +1,3 @@
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
